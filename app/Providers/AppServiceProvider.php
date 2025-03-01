5dab928ffef9bef8140cf43e1e9a09daa590e904
Frank Fava <frank.fava@gmail.com>
2025-03-01 14:55:24 +0100

    Custom DB Encryption Key

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,16 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Vite;
+use App\Rules\Slug;
+use Filament\Facades\Filament;
+use Illuminate\Auth\Notifications;
+use Illuminate\Database\Eloquent;
+use Illuminate\Encryption\Encrypter;
+use Illuminate\Support\Facades;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use Illuminate\Support\Stringable;
+use Illuminate\Validation\Rule;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +28,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
+        Facades\Vite::prefetch(concurrency: 3);
+
+        $this->changeDatabaseEncryptionKey();
+    }
+
+    /**
+     * Change the database encryption key.
+     *
+     * @see https://larasec.substack.com/p/security-tip-custom-encryption-key
+     */
+    protected function changeDatabaseEncryptionKey(): void
+    {
+        $key = config('database.encryption_key');
+        if (! $key) {
+            return;
+        }
+        $key = base64_decode(Str::after($key, 'base64:'));
+        Eloquent\Model::encryptUsing(new Encrypter($key, config('app.cipher')));
     }
 }
