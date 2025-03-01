4b241189eb5678b0ddb033aca82a839072a917ab
Frank Fava <frank.fava@gmail.com>
2025-03-01 16:41:08 +0100

    Spilt Routs files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,13 +3,28 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        using: function () {
+
+            // Api Guest
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+
+            // API - User
+            Route::middleware(['api', 'auth:api'])
+                ->prefix('api')
+                ->group(base_path('routes/api-auth.php'));
+
+            // Web
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
